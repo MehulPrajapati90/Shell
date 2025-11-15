992c9bb8e39af7aa0f61251b568b726b7b72f9cc
Mehul Prajapati <ailearn90@gmail.com>
2025-11-15 20:54:48 +0530

    added authentication

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,27 @@
 import express, { type Request, type Response } from "express";
 import dotenv from "dotenv";
+import { fromNodeHeaders, toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.use((req: Request, res: Response) => {
-    res.json({
-        message: "Hello world"
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
+app.all("/api/auth/*splat", toNodeHandler(auth))
+app.use(express.json());
+
+app.get('/api/me', async (req: Request, res: Response) => {
+    const session = await auth.api.getSession({
+        headers: fromNodeHeaders(req.headers)
     })
+    res.json(session);
 })
 
 app.listen(port, () => {
